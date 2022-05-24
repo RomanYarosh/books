d02637b7058e8bf5b355bfa52f5e1a8a30d02204
Yarosh Roman <yarosh0106@gmail.com>
2022-05-24 20:52:10 +0300

    Initial commit

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    author_name = models.CharField(max_length=255, blank=True, null=True)
+    year = models.IntegerField(blank=True, null=True)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Stores(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
