03e50c2d5653ed943cb2fbdc9c2c7af77fb26411
Yarosh Roman <yarosh0106@gmail.com>
2022-05-31 17:07:44 +0300

    .

--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
+from django.urls import reverse
+
 from . import models
+from . import forms
 
 
 def index(request):
@@ -16,17 +19,29 @@ def all_books(request):
     return render(request, 'all_books.html', {'context': context})
 
 
+def all_reviews(request):
+    reviews = models.Review.objects.all()
+    context = {
+        'reviews': reviews,
+    }
+    return render(request, 'all_reviews.html', {'context': context})
+
+
 def book_detail(request, book_id):
-    book = get_object_or_404(models.Book, id=book_id)
-    context = {
-        'book': book,
-    }
-    return render(request, 'book.html', {'context': context})
-
-
-def review_detail(request, review_id):
-    review = get_object_or_404(models.Review, id=review_id)
-    context = {
-        'review': review,
-    }
-    return render(request, 'review.html', {'context': context})
+    if request.method == 'POST':
+        form = forms.ReviewForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['text']
+            book = get_object_or_404(models.Book, id=book_id)
+            models.Review.objects.create(text=text, book=book)
+            return redirect(reverse('book', args=[book_id]))
+    else:
+        form = forms.ReviewForm()
+        book = get_object_or_404(models.Book, id=book_id)
+        reviews = models.Review.objects.filter(book=book_id)
+        context = {
+            'form': form,
+            'book': book,
+            'reviews': reviews,
+        }
+        return render(request, 'book.html', {'context': context})
